5a6b29f31165c7e1b74fbcf6d60099dd51e4ad6d
andrescanial <126804260+andrescanial@users.noreply.github.com>
2025-12-29 10:29:03 -0800

    Update auth.js

--- public/src/src/auth.js
+++ public/src/src/auth.js
@@ -1,1 +1,10 @@
+import CryptoJS from "https://cdn.jsdelivr.net/npm/crypto-js@4.1.1/+esm";
+import { USERS } from "../config/users.js";
+
+export function login(email, password) {
+  const hash = CryptoJS.SHA256(password).toString();
+  return USERS.find(
+    u => u.email === email && u.hash === hash
+  );
+}
 

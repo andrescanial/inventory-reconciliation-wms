31c45fe7a6f082306e98a83ad63abd1cfe76035d
andrescanial <126804260+andrescanial@users.noreply.github.com>
2025-12-29 10:13:28 -0800

    Update users.js

--- public/src/auth.js src/config/users.js
+++ public/src/auth.js src/config/users.js
@@ -1,1 +1,17 @@
+import CryptoJS from "https://cdn.jsdelivr.net/npm/crypto-js@4.1.1/+esm";
+
+export const USERS = [
+  {
+    email: "andres.canial@golocad.com",
+    hash: CryptoJS.SHA256("Andresanadon123!").toString()
+  },
+  {
+    email: "shielaferwelo@golocad.com",
+    hash: CryptoJS.SHA256("Shielaferwelo123!").toString()
+  },
+  {
+    email: "vincebaluyot22@golocad.com",
+    hash: CryptoJS.SHA256("Vincebaluyot123!").toString()
+  }
+];
 
